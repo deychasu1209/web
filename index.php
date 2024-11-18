a845146e02dc129b17c3bb81882e7b17e401ed9d
Deybi Paolo Chávez Sú <202014012@uns.edu.pe>
2024-11-18 11:21:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
-  <div class = "container">
+  <div class="container">
     <div class="jumbotron">
       <h1 class="display-4">Catálogo de clientes</h1>
       <p class="lead">Aplicación de muestra del catálogo de clientes</p>
@@ -16,32 +16,37 @@
     <table class="table table-striped table-responsive">
       <thead>
         <tr>
+          <th>ID</th>
           <th>Nombre</th>
           <th>Historial Crediticio</th>
-          <th>Direccion</th>
+          <th>Dirección</th>
           <th>Ciudad</th>
           <th>Provincia</th>
-          <th>Pais</th>
-          <th>Codigo Postal</th>
+          <th>País</th>
+          <th>Código Postal</th>
         </tr>
       </thead>
       <tbody>
         <?php
 
+        // Conexión a la base de datos MySQL
         $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
 
-        $cadenaSQL = "select * from s_cliente";
+        // Consulta SQL para seleccionar todos los registros de la tabla s_cliente
+        $cadenaSQL = "SELECT * FROM s_cliente";
         $resultado = mysqli_query($conexion, $cadenaSQL);
 
+        // Mostrar los registros en la tabla HTML
         while ($fila = mysqli_fetch_object($resultado)) {
-         echo "<tr><td> " .$fila->nombre . 
-         "</td><td>" . $fila->calificacion_crediticia .
-         "</td><td>" . $fila->direccion .
-         "</td><td>" . $fila->ciudad .
-         "</td><td>" . $fila->provincia .
-         "</td><td>" . $fila->pais .
-         "</td><td>" . $fila->codigo_postal .
-         "</td></tr>";
+         echo "<tr><td>" . $fila->id .
+              "</td><td>" . $fila->nombre .
+              "</td><td>" . $fila->calificacion_crediticia .
+              "</td><td>" . $fila->direccion .
+              "</td><td>" . $fila->ciudad .
+              "</td><td>" . $fila->provincia .
+              "</td><td>" . $fila->pais .
+              "</td><td>" . $fila->codigo_postal .
+              "</td></tr>";
        }
        ?>
      </tbody>
@@ -52,4 +57,3 @@
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
-
